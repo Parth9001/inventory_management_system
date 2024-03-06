f2592aefdc2ac3f5a7708282c59cb92b39cf4ef2
adityapatil2310 <adityamp23@gmail.com>
2024-03-06 20:12:55 +0530

    created login page

---
+++ frontend/src/Components/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='login'>
+        <div className="loginContent">
+            <span id='login'>Login</span>
+            <form action="">
+                <span id="email">Email</span><br />
+                <input type="email" name="email" id="" placeholder='Enter Email' required/><br />
+                <p className='pwd'>
+                    <span id="pwd">Password</span><br />
+                    <input type="password" name="password" id="" placeholder='Enter Password' required/>
+                </p>
+                <button id="loginBtn" type="submit">LOG IN</button>
+
+            </form>
+
+        </div>
+
+    </div>
+  )
+}
