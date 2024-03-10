c95720b4a720200cec317d1d50a4d12d6d6a6361
ojassomani <ojassomani@gmail.com>
2024-03-10 19:25:28 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Card from './Components/Card';
 import Login from './Components/Login';
 import Sidebar from './Components/sidebar';
-
+import MyComponent from './addPageComp/addPage';
 function App() {
   return (
     <>
       {/* <Login/> */}
       {/* <Card/> */}
-        <Sidebar />
+        {/*<Sidebar />*/}
+      <MyComponent />
         </>
   );
 }
