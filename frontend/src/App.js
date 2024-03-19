d0c3ad4acb2d8f4bda611e16baf8d4cfe555ac9f
Gaurav <gauravf64@gmail.com>
2024-03-20 02:39:16 +0530

    routing of sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,45 @@ import MyComponent from './addPageComp/addPage';
 import EditPage from './editPageComp/editPage';
 import AuditCard from './Components/AuditCard';
 import AuditPageBar from './Components/AuditPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Audit from './Components/AuditPage'
 
 function App() {
   return (
     <>
-      <Login/>
+      <div >
+        <Router>
+          <Switch>
+            <Route exact path="/" > 
+              <Pagebg/>
+              <div style={{marginLeft:'20%', paddingTop: '5%'}}>
+                <Home/>
+              </div>
+            </Route>
+            <Route exact path="/add" > 
+              <Pagebg/>
+              <div style={{marginLeft:'20%', paddingTop: '5%'}}>
+                <MyComponent/>
+              </div>
+            </Route>
+            <Route exact path="/issued" > 
+              <Pagebg/>
+              <div style={{marginLeft:'20%', paddingTop: '5%'}}>
+                <Issued/>
+              </div>
+            </Route>
+            <Route exact path="/audit" > 
+              <Pagebg/>
+              <div style={{marginLeft:'20%', paddingTop: '5%'}}>
+                <Audit/>
+              </div>
+            </Route>
+           
+          </Switch>
+        </Router>
+      </div>
+      {/* <Login/> */}
       {/* <Card/> */}
-      {/* <Pagebg/> */}
-      {/* <div style={{marginLeft:'20%', paddingTop: '5%'}}> */}
         {/* React router dom --- */}
           {/* <Home/> */}
           {/* <MyComponent/> */}
@@ -24,7 +55,7 @@ function App() {
           {/* <AuditCard /> */}
         {/* <AuditPageBar /> */}
         {/* <Issued /> */}
-      {/* </div> */}
+      
     </>
   );
 }
