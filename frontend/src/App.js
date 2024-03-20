24617cc7ac82295ee4a6b6d5000daada38d44031
adityapatil2310 <adityamp23@gmail.com>
2024-03-20 16:50:36 +0530

    fixed audit page issue after routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,39 @@
 import './App.css';
-import Card from './Components/Card';
 import Issued from './Components/issued';
 import Login from './Components/Login';
 import Home from './Components/home';
 import Pagebg from './Components/pagebg'
 import MyComponent from './addPageComp/addPage';
 import EditPage from './editPageComp/editPage';
-import AuditCard from './Components/AuditCard';
-import AuditPageBar from './Components/AuditPage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Audit from './Components/AuditPage'
+import Audit from './Components/audit'
 
 function App() {
   return (
     <>
       <Router>
+          <Pagebg/>
           <Switch>
             <Route exact path="/" > 
-              <Pagebg/>
+              {/* <Pagebg/> */}
               <div style={{marginLeft:'20%', paddingTop: '5%'}}>
                 <Home/>
               </div>
             </Route>
             <Route exact path="/add" > 
-              <Pagebg/>
+              {/* <Pagebg/> */}
               <div style={{marginLeft:'20%', paddingTop: '5%'}}>
                 <MyComponent/>
               </div>
             </Route>
             <Route exact path="/issued" > 
-              <Pagebg/>
+              {/* <Pagebg/> */}
               <div style={{marginLeft:'20%', paddingTop: '5%'}}>
                 <Issued/>
               </div>
             </Route>
             <Route exact path="/audit" > 
-              <Pagebg/>
+              {/* <Pagebg/> */}
               <div style={{marginLeft:'20%', paddingTop: '5%'}}>
                 <Audit/>
               </div>
@@ -47,7 +45,7 @@ function App() {
       {/* <Pagebg/> */}
       {/* <div style={{marginLeft:'20%', paddingTop: '5%'}}> */}
         {/* React router dom --- */}
-          <Home/>
+          {/* <Home/> */}
           {/* <MyComponent/> */}
           {/* <EditPage/> */}
           {/* <Issued/> */}
