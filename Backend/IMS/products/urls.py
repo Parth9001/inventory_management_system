d6d28256a024b597d73bded7ca5a371d0f0c0ab5
MrunalVai <mvairagade44@gmail.com>
2024-03-21 21:14:46 +0530

    ss

--- Backend/IMS/products/urls.py
+++ Backend/IMS/products/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('user_add/',views.user_add),
     path('user_info/',views.user_list),
     path('put/<int:pk>',views.Put.as_view()),
-    # path('search/', views.search),
     path('search/', views.SearchAPIView.as_view(), name='search'),
     path('api/login/', views.LoginAPIView.as_view(), name='login'),
     path('', views.get_info),
+    path('issue_product/',views.issue_product),
+    path('user_products_issued/',views.get_user_products),
 ]
